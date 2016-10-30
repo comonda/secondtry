6e077ee55f37286f0e3af71b026d81da8933b4d5
mac <mac@localhost.local>
2016-10-30 15:23:27 +0800

    day-10

--- www/apis.py
+++ www/apis.py
@@ -13,18 +13,19 @@ class APIError(Exception):
     '''
     the base APIError which contains error(required), data(optional) and message(optional).
     '''
-    def __init__(self, error, data='', message=''):
+    def __init__(self, error, data, message=''):
         super(APIError, self).__init__(message)
         self.error = error
         self.data = data
         self.message = message
+        print ('data= %s, message= %s' % (self.data, self.message))
 
 class APIValueError(APIError):
     '''
     Indicate the input value has error or invalid. The data specifies the error field of input form.
     '''
     def __init__(self, field, message=''):
-        super(APIValueError, self).__init__('value:invalid', field, message)
+        super(APIValueError, self).__init__('value:error invalid', field, message)
 
 class APIResourceNotFoundError(APIError):
     '''
--- www/comapre.py
+++ www/comapre.py
@@ -125,4 +125,3 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
 
-
